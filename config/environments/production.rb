9053dbc64470bd40978af75e925b1e73672c7db1
Scott Watermasysk <scottwater@gmail.com>
2019-01-18 13:40:04 -0500

    setup for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # HEROKU Settings. No need for 12_factor gem
+# Disable serving static files from the `/public` folder by default since
+# Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
 end
